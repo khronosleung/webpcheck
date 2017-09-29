aced9ea984dcfff467cc88861fae5de8c3effa77
kidney <kidneyleung@gmail.com>
2017-09-29 15:52:09 +0800

    发布

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
 # WebP Check
 
-webp兼容性检查
+webp兼容性检查，检查过程一次性，不依赖网络
 
-用例：
+## 安装
+
+```shell
+npm install --save webpcheck
+```
+
+
+## 用例
 
 ```js
 import WebPCheck from 'webpcheck';
 
-if (WebPCheck) {
+if (WebPCheck()) {
     console.log('support');
 } else {
     console.log('not support');
--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
-import extend from 'extend';
-
 const TEST_IMAGES_BASE64 = {
     lossy: 'UklGRiIAAABXRUJQVlA4IBYAAAAwAQCdASoBAAEADsD+JaQAA3AAAAAA',
     lossless: 'UklGRhoAAABXRUJQVlA4TA0AAAAvAAAAEAcQERGIiP4HAA==',
     alpha: 'UklGRkoAAABXRUJQVlA4WAoAAAAQAAAAAAAAAAAAQUxQSAwAAAARBxAR/Q9ERP8DAABWUDggGAAAABQBAJ0BKgEAAQAAAP4AAA3AAP7mtQAAAA==',
     animation: 'UklGRlIAAABXRUJQVlA4WAoAAAASAAAAAAAAAAAAQU5JTQYAAAD/////AABBTk1GJgAAAAAAAAAAAAAAAAAAAGQAAABWUDhMDQAAAC8AAAAQBxAREYiI/gcA'
 };
-
-// ========== 配置
-const DEFAULT_CONFIG = {
-    resultStoreKey: 'webPCheckResult',
-    callback: () => {}
-};
-const CONFIG = extend(true, {}, DEFAULT_CONFIG, (window.WebPCheckCongfig || {}));
-
-delete window.WebPCheckCongfig;
 
 // ========== 常用函数
 function isType(s, typeString) {
@@ -23,11 +12,22 @@ function isType(s, typeString) {
 function isObject(s) {
     return isType(s, 'Object');
 }
+function isNull(s) {
+    return isType(s, 'Null');
+}
 function storageSetItem(val) {
-    localStorage.setItem(CONFIG.resultKey, val);
+    localStorage.setItem(STORAGE_KEY, val);
 }
 function storageGetItem() {
-    return localStorage.getItem(CONFIG.resultKey);
+    return localStorage.getItem(STORAGE_KEY);
+}
+function extend(target, source) {
+    for (let key in source) {
+        if (source.hasOwnProperty(key)) {
+            target[key] = source[key];
+        }
+    }
+    return target;
 }
 
 function load(caseItemName, cb) {
@@ -49,39 +49,45 @@ function load(caseItemName, cb) {
     img.src = `data:image/webp;base64,${TEST_IMAGES_BASE64[caseItemName]}`;
 }
 
+
+// ========== 配置
+const STORAGE_KEY = 'webPCheckResult';
+
 // ========== 检查流程
-let WebPCheck = null;
+let WebPCheckResult = null;
 
-let storageResult = JSON.parse(storageGetItem());
-if (isObject(storageResult)) {
-    WebPCheck = {
-        lossy: storageResult.lossy === true || false,
-        lossless: storageResult.lossless === true || false,
-        alpha: storageResult.alpha === true || false,
-        animation: storageResult.animation === true || false
-    };
-} else {
-    let testCases = ['lossy', 'lossless', 'alpha', 'animation'];
-    let caseItemName;
-    let totalCheck = testCases.length;
-    let currentCheck = 0;
-    while (caseItemName = testCases.shift()) {
-        load(caseItemName, (response) => {
-            ++currentCheck;
-
-            if (WebPCheck === null) {
-                WebPCheck = {};
-            }
-
-            WebPCheck[caseItemName] = response;
-
-            if (totalCheck === currentCheck) {
-                storageSetItem(JSON.stringify(WebPCheck));
-                CONFIG.callback(WebPCheck);
-            }
-        });
+export default function WebPCheck() {
+    if (!isNull(WebPCheckResult)) {
+        return WebPCheckResult;
     }
-}
 
+    let storageResult = JSON.parse(storageGetItem());
+    if (isObject(storageResult)) {
+        WebPCheckResult = {
+            lossy: storageResult.lossy === true || false,
+            lossless: storageResult.lossless === true || false,
+            alpha: storageResult.alpha === true || false,
+            animation: storageResult.animation === true || false
+        };
+    } else {
+        let testCases = ['lossy', 'lossless', 'alpha', 'animation'];
+        let caseItemName;
+        let totalCheck = testCases.length;
+        let currentCheck = 0;
+        while (caseItemName = testCases.shift()) {
+            load(caseItemName, (response) => {
+                ++currentCheck;
 
-export default WebPCheck;
+                if (isNull(WebPCheckResult)) {
+                    WebPCheckResult = {};
+                }
+
+                WebPCheckResult[caseItemName] = response;
+
+                if (totalCheck === currentCheck) {
+                    storageSetItem(JSON.stringify(WebPCheckResult));
+                }
+            });
+        }
+    }
+};
